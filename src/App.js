f5d6788f2a591cdc8e832415295d13d0bdaac51c
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-03-09 19:28:53 +0600

    fix some error

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Review from './components/Review/Review';
 import ProductDetails from './components/ProductDetails/ProductDetails';
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { useState } from 'react';
 import Cart from '../Cart/Cart';
 import Products from '../Products/Products';
@@ -8,7 +7,7 @@ import { addToDatabaseCart } from '../../utilities/databaseManager';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10)
-    const [products, setProducts] = useState(first10);
+    const [products] = useState(first10);
     const [cart, setCart] = useState([]);
 
     const addToCart = product => {
